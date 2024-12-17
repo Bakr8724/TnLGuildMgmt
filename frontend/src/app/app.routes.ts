2bb03c8cf27a28953e9baca3618585ccfd1731eb
bakr <alkayalibakr@gmail.com>
2024-12-17 15:08:32 -0600

    frontend components setup partial

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch:'full'}, //default route
+    {path: 'login', component: LoginPageComponent},
+    {path: 'profile', component: ProfileComponent},
+];
